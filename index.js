bcc01b44dd770b7187c5ea094613ba617881ed3d
Pham Van Ha <phamvanha@vnpt.vn>
2019-04-18 14:25:23 +0700

    delete all unecessary things

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-$("#flipbook").turn({
-    width: 600,
-    height: 500,
-    autoCenter: true
-});
-
-$("#flipbook").bind("turning", function(event, page, view) {
-    if (page==1) {
-        event.preventDefault(); //will not happen at page 1
-    }else{
-    }
+$( document ).ready(function() {
+    $("#flipbook").turn({
+        width: 400,
+        height: 300,
+        autoCenter: true
+    });
 });
